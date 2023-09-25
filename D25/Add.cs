a88c94bd6c12239f87f9ba54d305c616dce0ac15
Vaishali Ranjan <110801149+vaishaliranjan@users.noreply.github.com>
2023-09-25 12:19:48 +0530

    Updated File

---
+++ D25/Add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D25
+{
+    internal class Add
+    {
+       public Add() {
+            Console.WriteLine(2 + 3);
+        }
+    }
+}
---
+++ D26/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World2!");
+Console.WriteLine(2 + 3);
