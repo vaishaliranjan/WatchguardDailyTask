7373b7b0f594d1ec2439355026248b56fcba243c
Vaishali Ranjan <110801149+vaishaliranjan@users.noreply.github.com>
2023-10-20 18:22:27 +0530

    Exception handling & Async Functions

---
+++ CSharpAdvanced/ExceptionHandling.cs
@@ -0,0 +1,141 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharpAdvanced
+//{
+//    public class Calculator
+//    {
+//        public int divide(int numerator, int denominator)
+//        {
+//            return numerator / denominator;
+//        }
+//    }
+//    internal class ExceptionHandling
+//    {
+//        static void Main(string[] args)
+//        {
+//            StreamReader streamReader = null;
+
+//            try
+//            {
+//                //Calculator calculator = new Calculator();
+//                //calculator.divide(1, 0);
+//                streamReader = new StreamReader(@"c:\file.zip");
+
+//                var content = streamReader.ReadToEnd();
+//                throw new Exception("oops");
+
+//            }
+//            //catch(DivideByZeroException ex)
+//            //{
+//            //    Console.WriteLine("You cant divide by 0");
+//            //}
+//            //catch(ArithmeticException ex)
+//            //{
+
+//            //}
+//            catch(Exception ex)
+//            {
+//                Console.WriteLine("Sorry, an unexpected error occured!");
+//            }
+//            finally
+//            {
+//                //make sure that sream is closed
+//                if (streamReader != null)
+//                {
+//                    streamReader.Dispose();
+//                }
+//            }
+
+//        }
+//    }
+//}
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharpAdvanced
+//{
+//    public class Calculator
+//    {
+//        public int divide(int numerator, int denominator)
+//        {
+//            return numerator / denominator;
+//        }
+//    }
+//    internal class ExceptionHandling
+//    {
+//        static void Main(string[] args)
+//        {
+
+
+//            try
+//            {
+//                using (var streamReader = new StreamReader(@"c:\file.zip"))
+//                {
+//                    var content = streamReader.ReadToEnd();
+//                }
+//            }
+
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine("Sorry, an unexpected error occured!");
+//            }
+
+//        }
+//    }
+//}
+//****************************MANUALLY CREATING EXCEPTION *********************
+//using System;
+//namespace ExceptionHandling
+//{
+//    public class Video { }
+//    //Exception
+//    public class YoutubeException: Exception
+//    {
+//        public YoutubeException(string message, Exception ex) 
+//        :base(message, ex) { }
+        
+//    }
+//    public class YoutubeAPI
+//    {
+//        public List<Video> GetVideos(string user)
+//        {
+//            try
+//            {
+//                //Access yt web series
+//                //read the data 
+//                //create a list of video objects
+//                throw new Exception("OOps some low level youtube error!!");
+//            }
+//            catch(Exception ex)
+//            {
+//                //Log
+//                throw new YoutubeException("Couldnt fetch the videos from youtube.", ex);
+//            }
+//            return new List<Video>();
+//        }
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            try
+//            {
+//                YoutubeAPI api = new YoutubeAPI();
+//                var videos = api.GetVideos("vaishali");
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//            }
+            
+//        }
+//    }
+//}
---
+++ CSharpAdvanced/Multithreading.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced
+{
+    internal class Multithreading
+    {
+        public static void func1()
+        {
+            for(int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine("Func1= "+ i);
+            }
+        }
+
+        public static void func2()
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine("Func2= " + i);
+                if (i == 25)
+                {
+                    Console.WriteLine("Thread is going to sleep for 8 seconds.");
+                    Thread.Sleep(8000);
+                    //func3 will wait for func2 to complete
+                }
+            }
+        }
+
+        public static void func3()
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine("Func3= " + i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Only one thread -> runs sequentially
+            //func1();
+            //func2();
+            //func3();
+            //three threads for each function- so they will run simultaneously-> executed by the operating system using time sharing.-> each thread equally priority
+           Thread t1= new Thread(func1);
+            Thread t2= new Thread(func2);
+            Thread t3= new Thread(func3);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            //Thread t = Thread.CurrentThread;
+            //t.Name = "Main thread";
+            //Console.WriteLine("Current executing thread is: "+ Thread.CurrentThread.Name);
+        }
+    }
+}
